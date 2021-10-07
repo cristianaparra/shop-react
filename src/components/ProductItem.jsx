dce796468f2fcb605dd95914b52734658c12f82e
cristian parra <cristianparra@Mac-mini-de-cristian.local>
2021-10-07 16:57:37 -0300

    creando provider para el carrito

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,27 +1,28 @@
-import React,{useState} from 'react';
-import '@styles/ProductItem.scss';
-import add_to_cart from '@icons/bt_add_to_cart.svg'
+import React, { useContext } from "react";
+import AppContext from "@context/AppContext";
+import "@styles/ProductItem.scss";
+import add_to_cart from "@icons/bt_add_to_cart.svg";
 
-const ProductItem = ({product}) => {
-	const [cart, setCart] = useState([]);
-	const handleClick = ()=> {
-		setCart('hola mundo');
-	}
-	return (
-		<div className="ProductItem">
-			<img src={product.images[0]} alt={product.title} />
-			<div className="product-info">
-				<div>
-					<p>${product.price}</p>
-					<p>{product.title}</p>
-				</div>
-				<figure onClick={handleClick}>
-					<img src={add_to_cart} alt="" />
-				</figure>
-				
-			</div>
-		</div>
-	);
-}
+const ProductItem = ({ product }) => {
+  const { addToCart } = useContext(AppContext);
+
+  const handleClick = (item) => {
+    addToCart(item);
+  };
+  return (
+    <div className="ProductItem">
+      <img src={product.images[0]} alt={product.title} />
+      <div className="product-info">
+        <div>
+          <p>${product.price}</p>
+          <p>{product.title}</p>
+        </div>
+        <figure onClick={() => handleClick(product)}>
+          <img src={add_to_cart} alt="" />
+        </figure>
+      </div>
+    </div>
+  );
+};
 
 export default ProductItem;
